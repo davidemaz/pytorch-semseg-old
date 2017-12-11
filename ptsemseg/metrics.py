cde04745b1937e6ecdfa4703ae3715b6e7a3bf78
Davide Mazzini <davidemaz@gmail.com>
2017-12-11 18:02:14 +0100

    Changed some variables' names

--- ptsemseg/metrics.py
+++ ptsemseg/metrics.py
@@ -5,10 +5,10 @@ import numpy as np
 
 def _confusion_matrix(gt, pred, n_classes):
     mask = (gt >= 0) & (gt < n_classes)
-    hist = np.bincount(
+    cm = np.bincount(
         n_classes * gt[mask].astype(int) +
         pred[mask], minlength=n_classes**2).reshape(n_classes, n_classes)
-    return hist
+    return cm
 
 
 def scores(gts, preds, n_classes):
@@ -18,19 +18,19 @@ def scores(gts, preds, n_classes):
       - mean IU
       - fwavacc
     """
-    hist = np.zeros((n_classes, n_classes))
+    cm = np.zeros((n_classes, n_classes))
     for lt, lp in zip(gts, preds):
-        hist += _confusion_matrix(lt.flatten(), lp.flatten(), n_classes)
-    acc = np.diag(hist).sum() / hist.sum()
-    acc_cls = np.diag(hist) / hist.sum(axis=1)
+        cm += _confusion_matrix(lt.flatten(), lp.flatten(), n_classes)
+    pixel_acc = np.diag(cm).sum() / cm.sum()
+    acc_cls = np.diag(cm) / cm.sum(axis=1)
     acc_cls = np.nanmean(acc_cls)
-    iu = np.diag(hist) / (hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist))
+    iu = np.diag(cm) / (cm.sum(axis=1) + cm.sum(axis=0) - np.diag(cm))
     mean_iu = np.nanmean(iu)
-    freq = hist.sum(axis=1) / hist.sum()
+    freq = cm.sum(axis=1) / cm.sum()
     fwavacc = (freq[freq > 0] * iu[freq > 0]).sum()
     cls_iu = dict(zip(range(n_classes), iu))
 
-    return {'Overall Acc: \t': acc,
+    return {'Pixel Acc: \t': pixel_acc,
             'Mean Acc : \t': acc_cls,
             'FreqW Acc : \t': fwavacc,
             'Mean IoU : \t': mean_iu,}, cls_iu
